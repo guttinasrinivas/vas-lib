858ab6898b6476885d3f0f554e03bf2da971d6e3
Vasu <guttina.srinivas@gmail.com>
2026-07-21 15:18:13 +1000

    Add Python type arg parser for C++.
    Similar to CLI11, but lighter weight and fully managed memory.

---
+++ tests/cli_args_test.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include "cli_args.hpp"
+
+using std::cout;
+using std::endl;
+
+
+void usage(char *av[], CLI_Args& ap)
+{
+    cout << endl << "Usage: " << av[0];
+    cout << " " << *ap.exp_args();
+    cout << endl << *ap.help();
+    cout << endl;
+}
+
+
+int main(int ac, char *av[])
+{
+
+    cout << ac << "Parsing CLI Args: ";
+    for (int ii = 0; ii < ac; ii++)
+    {
+        cout << endl << av[ii];
+    }
+
+    CLI_Args ap(ac, av);
+    auto in_all = ap.add_flag("-a");
+    auto in_long = ap.add_flag("-l");
+    auto in_format = ap.add_str("-F");
+    auto in_inval = ap.add_str("--in-val");
+
+    try
+    {
+        cout << endl << "<-------- Parsed CLI Args -------->\n";
+        ap.parse_args();
+
+        cout << endl << "in_all: " << *in_all;
+        cout << endl << "in_long: " << *in_long;
+        cout << endl << "in_format: " << *in_format;
+        cout << endl << "in_val: " << *in_inval;
+        cout << endl << "unnamed args: ";
+        for (auto inarg: ap.get_args())
+        {
+            cout << *inarg << ", ";
+        }
+
+        cout << endl << ">-------- Parsed CLI Args --------<\n";
+    }
+    catch (const std::exception& exc)
+    {
+        cout << exc.what();
+        usage(av, ap);
+        cout << endl << "!-------- Parsed CLI Args --------!\n";
+    }
+
+    cout << std::endl;
+
+    return 0;
+}
+
+/* End of file */
